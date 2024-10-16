020daf66e5a9a4c09e550c2e53f2c96aada0b206
sAbA-pixel-3 <seconddonutbs@gmail.com>
2024-10-16 18:10:59 +0600

    Add files via upload

---
+++ if.py
@@ -0,0 +1,140 @@
+# if elif else - операторы условия
+
+# if 5>6:
+    # print('code True')
+
+# if 'hello' == 'Hello':
+    # print(True)
+
+# password = input('Enter a pass: ')
+# # if password == 'qwerty123':
+#     print('Correct')
+# else:
+#     print('Incorrect')
+
+# if 7>8:
+#     print('True')
+# elif 5>4:
+#     print('True')
+# else:
+#     print('nothing is correct')
+
+# if 5 == 5:
+#     if 5>4:
+#         print('hello')
+#     else:
+#         print('ne hello') 
+# else:
+#     print('qw')
+
+
+###############################################################
+# ОПЕРАТОРЫ if:
+
+# and
+# if 10 > 9 and 5 > 3 and 5<7 and 8>7 and 10>2: # оба должны быть True
+    # print('True') 
+# else:
+#     print('False') 
+
+
+# or
+# if 1>9 or 5>3: # надо чтоб хотя бы одно условие было True
+#     print('True')
+# else:
+#     print('False')   
+
+
+# in 
+# name = 'jon' # не работает на int, float, bool
+# if 'n' and 'o' 'j' in name:
+#     print('yes')
+# else:
+#     print('no') 
+
+
+# not
+# name = 'Hello'
+# if 'e' not in name:
+#     print('yes')
+# else:
+#     print('no') 
+
+# if 5 != 4: 
+#     print('yes')
+# else:
+#     print('no') 
+
+
+
+
+
+
+
+
+
+
+
+
+# q = int(input('Enter a number: '))
+# if q % 2 == 0:
+#     print("Четное и не делится на 3")
+# elif q / 3:
+#     print('Нечетное')
+
+# q = int(input("enter your age: "))
+# if q <= 13:
+#     print("baby")
+# elif q <= 17:
+#     print("teenager")
+# elif q == 100:
+#     print("zombie")
+# else:
+#     print("adult")
+
+# q = int(input("enter your year of birth: "))
+# if q % 4 == 0:
+#     print("Вы родились в високосный год!")
+# else:
+#     print("Oууу, не повезло!")
+
+# q = int(input("enter 1 number: "))
+# w = int(input("enter 2 number: "))
+# e = int(input("enter 3 number: "))
+# num = [q, w, e]
+# num.sort()
+# print("Numbers in ascending order: ", num)
+
+# q = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9]
+# if len(q) == len(set(q)):
+#     print("Only unique elements are contained! ")
+# else:
+#     print("Not every element is unique! ")
+
+# age = int(input("enter your age: "))
+# has_license = input("do you have griver's license by any chance?(yes/no): ").strip().lower()
+# if age >= 18 and has_license == "yes":
+#     print("You can surely drive a car!")
+# elif age >= 18 and has_license == "no":
+#     print("You cannot drive a car yet! \nFirst you need to get a driver's license!")
+# elif age <= 18 and has_license == "yes":
+#     print("You cannot drive a car yet! \nYou're not 18!")
+# else:
+#     print("You cannot drive a car yet!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
